5996f7961fe307f58598f95b7855526524588a34
Vineet Jadhav <vineet290403@gmail.com>
2025-05-29 23:59:20 +0530

    Fixes in Server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const process = require('process');
 const { authenticate } = require('@google-cloud/local-auth');
 const { google } = require('googleapis');
 const { auth } = require('google-auth-library');
+const { SpacesServiceClient } = require('@google-apps/meet').v2;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
